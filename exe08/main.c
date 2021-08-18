212dd350fe95aab8929bf82ca483b97ac5be4029
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2021-08-18 17:20:00 -0300

    @

--- exe08/main.c
+++ exe08/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+
+    int n[10];
+    int cont = 0;
+
+    //input
+    for(int i = 0; i < 10; i++)
+        scanf("%d",&n[i]);
+
+    //operation and result
+    while(n[cont] != 0){
+
+        if(n[cont]>0){
+            printf("\nPOSITIVO");
+        }else{
+            printf("\nNEGATIVO");
+        }
+
+        cont++;
+
+    }
+
+    return 0;
 }
