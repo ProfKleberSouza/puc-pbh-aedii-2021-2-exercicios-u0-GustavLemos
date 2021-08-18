e737ecdfbf57fbd3f2912a67fa6ccb4dc04c9611
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2021-08-18 19:03:59 -0300

    @

--- exe09/main.c
+++ exe09/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+//ERRO NAS TRES MAIORES NOTAS E NAO ESTA IMPRIMINDO O NOME
+
+int main(){
+
+    int nSize = 0;
+    float maior = 0;
+    float nomeApoio[30];
+    float media = 0;
+
+    typedef struct {
+        char nome[30];
+        float nota;
+    }aluno;
+
+    //input nSize
+    scanf("%d",&nSize);
+
+    aluno alunos[nSize];
+    aluno alunosTresMaioresNotas[3];
+
+    // inputs in array
+    for(int i = 0; i < nSize; i++){
+       scanf("\n%s %f",&alunos[i].nome,&alunos[i].nota);
+    }
+
+    //operations
+    for(int x = 0; x < nSize; x++){
+        for(int j = 0; j < nSize; j++){
+            if(alunos[j].nota>maior){
+                maior = alunos[j].nota;
+                //strcpy(nomeApoio,alunos[j].nome);
+            }
+        }
+
+        alunosTresMaioresNotas[x].nota= maior;
+       // strcpy(alunosTresMaioresNotas[x].nome, nomeApoio);
+
+    }
+
+
+    for(int k = 0; k < nSize ; k++){
+        media = media + alunos[k].nota;
+    }
+
+    media = media/nSize;
+
+    //results
+    printf("NOTA MEDIA = %.2f",media);
+
+    for(int m = 0; m < 3; m++){
+        printf("\n%s %f",alunosTresMaioresNotas[m].nome,alunosTresMaioresNotas[m].nota);
+    }
+
+    return 0;
 }
