3efe778e9fab22ffd03f52091cf04e37d02d635a
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2021-08-18 14:35:30 -0300

    Update main.c

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main (){
+
+    int t = 0;
+    int v = 0;
+    int r = 0;
+    int d = 0;
+    int l = 0;
+
+    //input
+    scanf("%d %d %d",&t,&v,&r);
+
+    //operations
+    d= t*v;
+    l= d/r;
+
+    //result
+    printf("R = %d\nV = %d\nT = %d\nD = %d\nL = %d\n",r,v,t,d,l);
+
+
+
+    return 0;
 }
